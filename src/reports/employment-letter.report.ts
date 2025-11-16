903e9d41a6a6c92b2ae33d6b2e30ef8a26f04861
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2025-11-16 15:50:21 -0600

    [add] component header

--- src/reports/employment-letter.report.ts
+++ src/reports/employment-letter.report.ts
@@ -1,4 +1,5 @@
-import type { Content, StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { headerSection } from './sections/header.section';
 
 const styles: StyleDictionary = {
     title: {
@@ -24,25 +25,11 @@ const styles: StyleDictionary = {
     },
 };
 
-const logo:Content={
-    image:'src/assets/tucan-code-logo.png',
-    width:100,
-    height:100,
-    alignment:'center',
-    margin:[0,0,0,20],
-}
-
 export const getEmploymentLetterReport = (): TDocumentDefinitions => {
     const docDefinition: TDocumentDefinitions = {
         styles: styles,
         pageMargins:[40,60,40,60],
-        header:{
-            columns:[logo,{
-                text:`${new Date()}`,
-                alignment:'right',
-                margin:[20,20],
-            }],
-        },
+        header:headerSection({showLogo:true,showDate:true}),
         content: [
             {
                 text: 'CONSTANCIA DE EMPLEO',
