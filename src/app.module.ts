a94d0b80c596ad67413ab92bb35ed563a44dc48e
Jonathan Alberto Barrera <jonabarreraabc@gmail.com>
2025-11-15 22:48:24 -0600

    [ADD] conexion a bd con sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { BasicReportsModule } from './basic-reports/basic-reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+      SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: '123456',
+      database: 'postgres',
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      logging: console.log,
+    }),
+      BasicReportsModule,
+  ],
 })
 export class AppModule {}
