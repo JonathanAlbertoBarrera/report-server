570f56f374bd8adf13ea94039e45d1342d0df61c
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-11-19 11:21:14 -0600

    [add] reporte con grafico donut y barras

--- src/store/reports/definitionChartSVgReport.ts
+++ src/store/reports/definitionChartSVgReport.ts
@@ -1,26 +1,100 @@
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import fs from 'fs';
+import * as Utils from '../../helpers/chart-utils';
 
 const svgContent = fs.readFileSync('src/printer-PDF/assets/ford.svg', 'utf8');
 
-export const definitionSvgBasicReport = (): TDocumentDefinitions => {
-  const docDefinition: TDocumentDefinitions = {
-    content: [
+const generateChartImage = async () => {
+  const chartConfig = {
+    type: 'bar', // Show a bar chart
+    data: {
+      labels: [2012, 2013, 2014, 2015, 2016], // Set X-axis labels
+      datasets: [
+        {
+          label: 'Users', // Create the 'Users' dataset
+          data: [120, 60, 50, 180, 120], // Add data to the chart
+        },
+      ],
+    },
+  };
+
+  return Utils.chartJsToImage(chartConfig);
+};
+
+const generateDonut = async () => {
+
+    // data que se manejara
+  const DATA_COUNT = 5;
+  const NUMBER_CFG = { count: DATA_COUNT, min: 0, max: 100 };
+
+  const data = {
+    labels: ['Red', 'Orange', 'Yellow', 'Green', 'Blue'],
+    datasets: [
       {
-        stack: [
-          {
-            text: 'SVG Chart Report',
-            alignment: 'center',
-          },
-          {
-            svg: svgContent,
-            fit: [150, 100],
-            alignment: 'center',
-          },
-        ],
+        label: 'Dataset 1',
+        data: Utils.numbers(NUMBER_CFG),
+        backgroundColor: Object.values(Utils.CHART_COLORS),
       },
     ],
   };
 
-  return docDefinition;
+  //configuracion del grafico de chartsjs
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+        title: {
+            display: true,
+            text: 'TITULO DONUT PRO',
+          },
+    },
+  };
+
+    return Utils.chartJsToImage(config);
 };
+
+export const definitionSvgBasicReport =
+  async (): Promise<TDocumentDefinitions> => {
+    
+    const [chart,chartDonut]=await Promise.all([generateChartImage(),generateDonut()]);
+    //const chart = await generateChartImage();
+    //const chartDonut=await generateDonut();
+
+    const docDefinition: TDocumentDefinitions = {
+      content: [
+        {
+          stack: [
+            {
+              text: 'SVG Chart Report',
+              alignment: 'center',
+            },
+            {
+              svg: svgContent,
+              fit: [150, 100],
+              alignment: 'center',
+            },
+            {
+              text: 'Grafica barras',
+              alignment: 'center',
+            },
+            {
+              image: chart,
+              width: 250,
+              alignment: 'center',
+            },
+            {
+                text: 'Grafica DONUT',
+                alignment: 'center',
+            },
+            {
+                image: chartDonut,
+                width: 250,
+                alignment: 'center',
+              },
+          ],
+        },
+      ],
+    };
+
+    return docDefinition;
+  };
--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { definitionHelloWorldReport } from 'src/employee/reports';
 import { PrinterService } from 'src/printer-PDF/printer.service';
 import { definitionSvgBasicReport } from './reports';
 
@@ -9,8 +8,8 @@ export class StoreService {
     constructor(private readonly printerService:PrinterService) {}
 
     async getSvgChart(){
-        const docDefinition=definitionSvgBasicReport();
-                    const doc=this.printerService.createPdf(docDefinition);
-                    return doc;
+        const docDefinition=await definitionSvgBasicReport();
+        const doc=this.printerService.createPdf(docDefinition);
+        return doc;
     }
 }
