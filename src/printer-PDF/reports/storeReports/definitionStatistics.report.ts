347d861e29c3e9639aedded08648fe398f9ac8ad
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-11-20 14:03:48 -0600

    [fix] reporte de estadisticas

--- src/printer-PDF/reports/storeReports/definitionStatistics.report.ts
+++ src/printer-PDF/reports/storeReports/definitionStatistics.report.ts
@@ -1,5 +1,5 @@
-import type { TDocumentDefinitions } from "pdfmake/interfaces"
-import { generateDonutChart, PositionLegend } from "src/printer-PDF/charts";
+import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { generateDonutChart, PositionLegend } from 'src/printer-PDF/charts';
 
 interface TopCountry {
     country: string;
@@ -14,47 +14,62 @@ interface ReportOptions {
     titleChart?: string;
 }
 
-export const definitionStatisticsReport = async (options: ReportOptions): Promise<TDocumentDefinitions> => {
-
+export const definitionStatisticsReport = async (
+    options: ReportOptions,
+): Promise<TDocumentDefinitions> => {
     //----options del reporte general
     const titleReport = options.titleReport || 'Reporte de Estadísticas';
     const subTitleReport = options.subTitleReport || 'Subtitle de Estadísticas';
     //----options respecto al grafico
     const titleChart = options.titleChart || null; //podria ser que no venga titulo ya que la fuente es de chart.js y puede utilizarse mejor un text para usar nuestras tipografias
-    const positionLegendChart = options.positionLegendChart || PositionLegend.RIGHT;
+    const positionLegendChart =
+        options.positionLegendChart || PositionLegend.RIGHT;
     const entriesChart = options.entriesChart;
 
-
     //para generar donut chart ocupamos de las opciones: entriesChart, positionLegendChart, titleChart
-    const donutGraphic = await Promise.resolve(generateDonutChart({
-        ListEntries: entriesChart.map(tc => ({
-            label: tc.country,
-            value: tc.customers,
-        })),
-        positionLegend: positionLegendChart,
-        titleChart: titleChart,
-    }));
-
+    const donutGraphic = await Promise.resolve(
+        generateDonutChart({
+            ListEntries: entriesChart.map((tc) => ({
+                label: tc.country,
+                value: tc.customers,
+            })),
+            positionLegend: positionLegendChart,
+            titleChart: titleChart,
+        }),
+    );
 
     const docDefinition: TDocumentDefinitions = {
         content: [
-            
             {
-                stack: [
+                columns: [
                     {
-                        text: titleReport, alignment: 'center',
+                        stack: [
+                            {
+                                text: '10 países con más clientes',
+                                alignment: 'center',
+                                margin: [0, 0, 0, 10],
+                            },
+                            {
+                                image: donutGraphic,
+                                width: 320,
+                            },
+                        ],
                     },
                     {
-                        text: subTitleReport, alignment: 'center',
+                        layout: 'lightHorizontalLines',
+                        width: 'auto',
+                        table: {
+                            headerRows: 1,
+                            widths: [100, 'auto'],
+                            body: [
+                                ['País', 'Clientes'],
+                                ...entriesChart.map((c) => [c.country, c.customers]),
+                            ],
+                        },
                     },
-                    {
-                        image: donutGraphic,
-                        width: 250,
-                        alignment: 'center',
-                    },
-                ]
-            }
+                ],
+            },
         ],
-    }
+    };
     return docDefinition;
-}
+};
